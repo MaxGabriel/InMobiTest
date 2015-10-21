c85e39be34ed72fde3e5538bef4e00d033d3f03a
Maximilian Tagher <feedback.tagher@gmail.com>
2015-10-20 17:45:14 -0700

    Initial Commit

---
+++ InMobiTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InMobiTest
+//
+//  Created by Maximilian Tagher on 10/20/15.
+//  Copyright © 2015 Heyzap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InMobiTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  InMobiTest
+//
+//  Created by Maximilian Tagher on 10/20/15.
+//  Copyright © 2015 Heyzap. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
